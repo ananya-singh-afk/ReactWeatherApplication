65a8b8eb15db8bf3c5c53035abd5aedd709ddecd
Ananya Singh <singh.ananya2580@gmail.com>
2024-11-08 12:25:22 +0530

    Add files via upload

---
+++ react-weather-app/src/components/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+        'x-rapidapi-key': '8bd20211ddmsh3c2d1ea844b8456p127c71jsnf83aea519726',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
